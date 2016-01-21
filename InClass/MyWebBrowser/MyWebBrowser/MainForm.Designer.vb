5417383d3d8a0a85a25f9b07cb3abd7c3521336e
Harrison Lingren <harrisonlingren@gmail.com>
2016-01-21 10:49:10 -0500

    initial commit, MyWebBrowser in-class project

---
+++ InClass/MyWebBrowser/MyWebBrowser/MainForm.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.wbrDisplay = New System.Windows.Forms.WebBrowser()
+        Me.txtURL = New System.Windows.Forms.TextBox()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.btnForw = New System.Windows.Forms.Button()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.stsStrip = New System.Windows.Forms.StatusStrip()
+        Me.stsLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'wbrDisplay
+        '
+        Me.wbrDisplay.Location = New System.Drawing.Point(12, 44)
+        Me.wbrDisplay.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.wbrDisplay.Name = "wbrDisplay"
+        Me.wbrDisplay.ScriptErrorsSuppressed = True
+        Me.wbrDisplay.Size = New System.Drawing.Size(631, 399)
+        Me.wbrDisplay.TabIndex = 0
+        '
+        'txtURL
+        '
+        Me.txtURL.Location = New System.Drawing.Point(56, 12)
+        Me.txtURL.Name = "txtURL"
+        Me.txtURL.Size = New System.Drawing.Size(448, 20)
+        Me.txtURL.TabIndex = 1
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(12, 10)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(38, 23)
+        Me.btnGo.TabIndex = 2
+        Me.btnGo.Text = "Go"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'btnForw
+        '
+        Me.btnForw.Location = New System.Drawing.Point(607, 10)
+        Me.btnForw.Name = "btnForw"
+        Me.btnForw.Size = New System.Drawing.Size(36, 23)
+        Me.btnForw.TabIndex = 2
+        Me.btnForw.Text = "-->"
+        Me.btnForw.UseVisualStyleBackColor = True
+        '
+        'btnBack
+        '
+        Me.btnBack.Location = New System.Drawing.Point(568, 10)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(36, 23)
+        Me.btnBack.TabIndex = 2
+        Me.btnBack.Text = "<--"
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Location = New System.Drawing.Point(510, 10)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(52, 23)
+        Me.btnRefresh.TabIndex = 2
+        Me.btnRefresh.Text = "Refresh"
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'stsStrip
+        '
+        Me.stsStrip.Location = New System.Drawing.Point(0, 452)
+        Me.stsStrip.Name = "stsStrip"
+        Me.stsStrip.Size = New System.Drawing.Size(655, 22)
+        Me.stsStrip.TabIndex = 3
+        '
+        'stsLabel
+        '
+        Me.stsLabel.AutoSize = True
+        Me.stsLabel.Location = New System.Drawing.Point(8, 457)
+        Me.stsLabel.Name = "stsLabel"
+        Me.stsLabel.Size = New System.Drawing.Size(33, 13)
+        Me.stsLabel.TabIndex = 4
+        Me.stsLabel.Text = "Done"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(655, 474)
+        Me.Controls.Add(Me.stsLabel)
+        Me.Controls.Add(Me.stsStrip)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Controls.Add(Me.btnBack)
+        Me.Controls.Add(Me.btnForw)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.txtURL)
+        Me.Controls.Add(Me.wbrDisplay)
+        Me.Name = "MainForm"
+        Me.Text = "My Web Browser"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents wbrDisplay As WebBrowser
+    Friend WithEvents txtURL As TextBox
+    Friend WithEvents btnGo As Button
+    Friend WithEvents btnForw As Button
+    Friend WithEvents btnBack As Button
+    Friend WithEvents btnRefresh As Button
+    Friend WithEvents stsStrip As StatusStrip
+    Friend WithEvents stsLabel As Label
+End Class
---
+++ InClass/MyWebBrowser/MyWebBrowser/MainForm.vb
@@ -0,0 +1,63 @@
+﻿'Author:        Harrison Lingren
+'Date:          2016-01-21
+'Project Name:  MyWebBrowser
+'Project Desc:  A simple windows application that implements
+'               a web browser with go, back, forward, refresh,
+'               and URL conversion functionality
+
+
+Public Class MainForm
+    'Navigates to the inputted Uri
+    Private Sub btnGO_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        wbrDisplay.Navigate(CheckURL(txtURL.Text))
+    End Sub
+
+    'Refreshes the web panel
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        wbrDisplay.Refresh()
+    End Sub
+
+    'Goes back in the web panel
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        wbrDisplay.GoBack()
+    End Sub
+
+    'Goes forward in the web panel
+    Private Sub btnForw_Click(sender As Object, e As EventArgs) Handles btnForw.Click
+        wbrDisplay.GoForward()
+    End Sub
+
+    'Checks the URL to make sure it includes 'http://'
+    Private Function CheckURL(url As String)
+        If url.StartsWith("http://") Then
+            Return New Uri(url)
+        Else
+            url = "http://" & url
+            Return New Uri(url)
+        End If
+    End Function
+
+    'Updates the status label
+    Private Sub wbrDisplay_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles wbrDisplay.DocumentCompleted
+        txtURL.Text = wbrDisplay.Url.ToString
+        stsLabel.Text = "Done"
+    End Sub
+
+    'Updates the status label
+    Private Sub wbrDisplay_Navigating(sender As Object, e As WebBrowserNavigatingEventArgs) Handles wbrDisplay.Navigating
+        stsLabel.Text = "Loading"
+    End Sub
+
+    'Resizes the window components
+    Private Sub MainForm_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        wbrDisplay.Size = New Size(Me.Size.Width, Me.Size.Height - 50)
+    End Sub
+
+    'Goes to the URL on enter keypress
+    Private Sub txtURL_KeyDown(sender As Object, e As KeyEventArgs) Handles txtURL.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            wbrDisplay.Navigate(CheckURL(txtURL.Text))
+        End If
+    End Sub
+
+End Class
