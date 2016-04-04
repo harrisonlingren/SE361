024773c0466ab679ba1ae1610b45530abeba652a
Harrison Lingren <harrisonlingren@gmail.com>
2016-04-03 21:42:05 -0400

    rename to entity

---
+++ Projects/TeamProject/TeamProject/Entity/cAppointment.vb
@@ -0,0 +1,39 @@
+﻿Public Class cAppointment
+
+    Private apptID As Integer
+    Private apptDate As String
+    Private apptAddr As String
+
+    Public Sub New()
+        apptID = 0
+        apptDate = String.Empty
+        apptAddr = String.Empty
+    End Sub
+
+    Public Property id As Integer
+        Get
+            Return apptID
+        End Get
+        Set(ByVal value As Integer)
+            apptID = value
+        End Set
+    End Property
+
+    Public Property datetime As String
+        Get
+            Return apptDate
+        End Get
+        Set(value As String)
+            apptDate = value
+        End Set
+    End Property
+
+    Public Property address As String
+        Get
+            Return apptAddr
+        End Get
+        Set(value As String)
+            apptAddr = value
+        End Set
+    End Property
+End Class
---
+++ Projects/TeamProject/TeamProject/Entity/cInvoice.vb
@@ -0,0 +1,51 @@
+﻿Public Class cInvoice
+
+    Private invoiceID As Integer
+    Private invoiceDate As String
+    Private invoiceCost As Decimal
+    Private invoiceDueDate As String
+
+    Public Sub New()
+        invoiceID = 0
+        invoiceDate = String.Empty
+        invoiceCost = 0.0
+        invoiceDueDate = String.Empty
+    End Sub
+
+    Public Property id As Integer
+        Get
+            Return invoiceID
+        End Get
+        Set(ByVal value As Integer)
+            invoiceID = value
+        End Set
+    End Property
+
+    Public Property datetime As String
+        Get
+            Return invoiceDate
+        End Get
+        Set(value As String)
+            invoiceDate = value
+        End Set
+    End Property
+
+    Public Property cost As Decimal
+        Get
+            Return invoiceCost
+        End Get
+        Set(value As Decimal)
+            invoiceCost = value
+        End Set
+    End Property
+
+    Public Property duedate As String
+        Get
+            Return invoiceDueDate
+        End Get
+        Set(value As String)
+            invoiceDueDate = duedate
+        End Set
+    End Property
+
+End Class
