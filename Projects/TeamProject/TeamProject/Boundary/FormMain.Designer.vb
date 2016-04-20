aa0246ee6de1414743c271c4cc0850ac4ee04581
harrisonlingren <harrisonlingren@gmail.com>
2016-04-20 19:25:12 -0400

    updated FormMain design

--- Projects/TeamProject/TeamProject/Boundary/FormMain.Designer.vb
+++ Projects/TeamProject/TeamProject/Boundary/FormMain.Designer.vb
@@ -27,65 +27,131 @@ Partial Class FormMain
         Me.btnInvoice = New System.Windows.Forms.Button()
         Me.btnAppts = New System.Windows.Forms.Button()
         Me.btnEmployee = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'btnCust
         '
-        Me.btnCust.Location = New System.Drawing.Point(12, 12)
+        Me.btnCust.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnCust.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCust.Location = New System.Drawing.Point(6, 19)
         Me.btnCust.Name = "btnCust"
-        Me.btnCust.Size = New System.Drawing.Size(130, 42)
+        Me.btnCust.Size = New System.Drawing.Size(227, 42)
         Me.btnCust.TabIndex = 0
         Me.btnCust.Text = "Customers"
-        Me.btnCust.UseVisualStyleBackColor = True
+        Me.btnCust.UseVisualStyleBackColor = False
         '
         'btnJobs
         '
-        Me.btnJobs.Location = New System.Drawing.Point(151, 12)
+        Me.btnJobs.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnJobs.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnJobs.Location = New System.Drawing.Point(6, 67)
         Me.btnJobs.Name = "btnJobs"
-        Me.btnJobs.Size = New System.Drawing.Size(130, 42)
+        Me.btnJobs.Size = New System.Drawing.Size(227, 42)
         Me.btnJobs.TabIndex = 1
         Me.btnJobs.Text = "Jobs"
-        Me.btnJobs.UseVisualStyleBackColor = True
+        Me.btnJobs.UseVisualStyleBackColor = False
         '
         'btnInvoice
         '
-        Me.btnInvoice.Location = New System.Drawing.Point(151, 60)
+        Me.btnInvoice.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnInvoice.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnInvoice.Location = New System.Drawing.Point(6, 115)
         Me.btnInvoice.Name = "btnInvoice"
-        Me.btnInvoice.Size = New System.Drawing.Size(130, 42)
+        Me.btnInvoice.Size = New System.Drawing.Size(227, 42)
         Me.btnInvoice.TabIndex = 3
-        Me.btnInvoice.Text = "Payments / Invoices"
-        Me.btnInvoice.UseVisualStyleBackColor = True
+        Me.btnInvoice.Text = "Manage Payments"
+        Me.btnInvoice.UseVisualStyleBackColor = False
         '
         'btnAppts
         '
-        Me.btnAppts.Location = New System.Drawing.Point(12, 60)
+        Me.btnAppts.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnAppts.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAppts.Location = New System.Drawing.Point(6, 67)
         Me.btnAppts.Name = "btnAppts"
-        Me.btnAppts.Size = New System.Drawing.Size(130, 42)
+        Me.btnAppts.Size = New System.Drawing.Size(227, 42)
         Me.btnAppts.TabIndex = 2
-        Me.btnAppts.Text = "Appointments"
-        Me.btnAppts.UseVisualStyleBackColor = True
+        Me.btnAppts.Text = "Appointment Calendar"
+        Me.btnAppts.UseVisualStyleBackColor = False
         '
         'btnEmployee
         '
-        Me.btnEmployee.Location = New System.Drawing.Point(81, 109)
+        Me.btnEmployee.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnEmployee.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEmployee.Location = New System.Drawing.Point(6, 19)
         Me.btnEmployee.Name = "btnEmployee"
-        Me.btnEmployee.Size = New System.Drawing.Size(130, 42)
+        Me.btnEmployee.Size = New System.Drawing.Size(227, 42)
         Me.btnEmployee.TabIndex = 4
         Me.btnEmployee.Text = "Employees"
-        Me.btnEmployee.UseVisualStyleBackColor = True
+        Me.btnEmployee.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnCust)
+        Me.GroupBox1.Controls.Add(Me.btnJobs)
+        Me.GroupBox1.Controls.Add(Me.btnInvoice)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 135)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(239, 181)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Manage Customers"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnEmployee)
+        Me.GroupBox2.Controls.Add(Me.btnAppts)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(259, 135)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(239, 181)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Manage Company"
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Script MT Bold", 36.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Blue
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(486, 73)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Just the Job"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Calibri", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Blue
+        Me.Label2.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Label2.Location = New System.Drawing.Point(17, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(475, 50)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Management Software"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
         'FormMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(293, 165)
-        Me.Controls.Add(Me.btnEmployee)
-        Me.Controls.Add(Me.btnInvoice)
-        Me.Controls.Add(Me.btnAppts)
-        Me.Controls.Add(Me.btnJobs)
-        Me.Controls.Add(Me.btnCust)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(510, 328)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "FormMain"
         Me.Text = "FormMain"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -95,4 +161,8 @@ Partial Class FormMain
     Friend WithEvents btnInvoice As Button
     Friend WithEvents btnAppts As Button
     Friend WithEvents btnEmployee As Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 End Class
